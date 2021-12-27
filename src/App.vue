1237e7b78235a9ff973ecdc0b260be1f61fc07f0
Sergio C Garzon <scgarzonp@gmail.com>
2021-12-27 10:10:36 -0500

    Table of main page without data, finished

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <px-header />
+    <px-assets-table />
   </main>
 </template>
 
 <script>
 import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
   components: {
     PxHeader,
+    PxAssetsTable,
   },
 };
 </script>
