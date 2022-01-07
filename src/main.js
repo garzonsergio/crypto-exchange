bad7b21dc14a739b41d08f61863a36676e5a552f
Sergio C Garzon <scgarzonp@gmail.com>
2022-01-07 08:35:00 -0500

    Project Finished

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import { dollarFilter, percentFilter } from "@/filters";
+import Chart from "chart.js";
+import Chartick from "vue-chartkick";
+import { VueSpinners } from "@saeris/vue-spinners";
 
 import router from "@/router";
 
+Vue.use(Chartick.use(Chart));
+Vue.use(VueSpinners);
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
